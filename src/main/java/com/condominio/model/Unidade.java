58571f0964e8a40095297e318310e4a7e8053dc4
lucasviniciussilvaprofissional-hub <lucasviniciussilvaprofissional@gmail.com>
2026-04-14 00:27:49 -0300

    nova classe unidade

---
+++ src/main/java/com/condominio/model/Unidade.java
@@ -0,0 +1,9 @@
+package com.condominio.model;
+
+public class Unidade {
+    private String id;
+    private String bloco;
+    private String metragem;
+    private int vaga;
+    private String status;
+}
