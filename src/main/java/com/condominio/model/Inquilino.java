b432e14dbf564171d27b364cde1aca49d95b95e5
CaioMentornap <mentornap@gmail.com>
2026-04-16 23:23:07 -0300

    feat: adição das classes: Unidade, Morador e suas subclasses, pet e veiculo.

--- src/main/java/com/condominio/model/Inquilino.java
+++ src/main/java/com/condominio/model/Inquilino.java
@@ -2,5 +2,9 @@ package com.condominio.model;
 
 public class Inquilino extends Morador
 {
+    public Inquilino(int id, String nome, String cpf, String telefone, Unidade unidade)
+    {
+        super(id,nome,cpf,telefone,unidade);
+    }
 
 }
