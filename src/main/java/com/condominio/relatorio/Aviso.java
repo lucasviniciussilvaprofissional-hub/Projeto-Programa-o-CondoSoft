ff24bac8f8aa9dfa2f53bbdc3dda1cf775c2e8a7
lucasviniciussilvaprofissional-hub <lucasviniciussilvaprofissional@gmail.com>
2026-04-17 00:49:05 -0300

    feat: add das classes Aviso, Ocorrencia Visitante

---
+++ src/main/java/com/condominio/relatorio/Aviso.java
@@ -0,0 +1,53 @@
+package com.condominio.relatorio;
+import java.time.LocalDateTime;
+
+public class Aviso {
+    private int id;
+    private String titulo;
+    private String mensagem;
+    private LocalDateTime dataCriacao;
+    private String prioridade;
+
+    public Aviso(){
+        this.dataCriacao = LocalDateTime.now();
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public String getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(String prioridade) {
+        this.prioridade = prioridade;
+    }
+}
---
+++ src/main/java/com/condominio/relatorio/Ocorrencia.java
@@ -0,0 +1,68 @@
+package com.condominio.relatorio;
+
+import java.time.LocalDateTime;
+
+public class Ocorrencia {
+
+    private int id;
+    private String descricao;
+    private String tipo; // BARULHO, ELEVADOR, SEGURANCA
+    private String status; // ABERTA, EM_ANALISE, RESOLVIDA
+    private LocalDateTime dataRegistro;
+
+    private String unidade;
+
+    public Ocorrencia() {
+        this.dataRegistro = LocalDateTime.now();
+        this.status = "ABERTA";
+    }
+
+    // getters e setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getDataRegistro() {
+        return dataRegistro;
+    }
+    public void setDataRegistro(LocalDateTime dataRegistro){
+        this.dataRegistro = dataRegistro;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+}
