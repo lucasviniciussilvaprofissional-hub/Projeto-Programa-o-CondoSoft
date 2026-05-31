9b622cdb7f038477f4d96ca5d5f15281ecb948e3
lucasviniciussilvaprofissional-hub <lucasviniciussilvaprofissional@gmail.com>
2026-05-31 18:26:14 -0300

    ADD : FINANCEIRO AJUSTE E COMPLEMENTAÇÃO DE SUAS DEVIDAS OPÇÕES

--- src/main/java/com/condominio/controller/financeiro/GerarBoletoController.java
+++ src/main/java/com/condominio/controller/financeiro/GerarBoletoController.java
@@ -1,64 +1,73 @@
 package com.condominio.controller.financeiro;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class GerarBoletoController {
 
-    public void voltarHome(MouseEvent event) {
-        trocarTela(event, "/com/condominio/home-view.fxml");
+    @FXML private Label lblVencimento;
+    @FXML private Label lblDataDocumento;
+    @FXML private Label lblNossoNumero;
+    @FXML private Label lblValorDocumento;
+    @FXML private Label lblLinhaDigitavel;
+
+    @FXML
+    public void initialize() {
+        System.out.println("[CondoSoft] Gerando visualização em GerarBoletoController...");
+
+        LocalDate hoje = LocalDate.now();
+        LocalDate vencimento = hoje.plusDays(5);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        if (lblDataDocumento != null) lblDataDocumento.setText(hoje.format(formatter));
+        if (lblVencimento != null) lblVencimento.setText(vencimento.format(formatter));
+        if (lblNossoNumero != null) lblNossoNumero.setText("09/24018523-1");
+        if (lblValorDocumento != null) lblValorDocumento.setText("R$ 350,00");
+        if (lblLinhaDigitavel != null) lblLinhaDigitavel.setText("34191.79001 01043.513184 91020.150008 7 97330000035000");
     }
 
-    public void voltarFinanceiro(ActionEvent event) {
-        trocarTela(event, "/com/condominio/financeiro/financeiro-view.fxml");
+    @FXML
+    private void imprimirBoleto(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("CondoSoft - Impressão");
+        alert.setHeaderText(null);
+        alert.setContentText("Enviando documento para a impressora padrão...");
+        alert.showAndWait();
     }
 
-    public void voltarFinanceiro(MouseEvent event) {
-        trocarTela(event, "/com/condominio/financeiro/financeiro-view.fxml");
-    }
+    @FXML
+    private void voltarHome(ActionEvent event) {
+        String[] caminhosHome = {
+                "/com/condominio/home-view.fxml",
+                "/com/condominio/unidade/home-view.fxml",
+                "/home-view.fxml"
+        };
 
-    public void preencherDadosUnidade(ActionEvent event) {}
-
-    public void calcularTotal() {}
-
-    public void limparFormulario(ActionEvent event) {}
-
-    public void gerarBoleto(ActionEvent event) {}
-
-    private void trocarTela(ActionEvent event, String caminhoFXML) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource(caminhoFXML));
-
-            Stage stage = (Stage) ((Node) event.getSource())
-                    .getScene()
-                    .getWindow();
-
-            stage.setScene(new Scene(root));
-            stage.show();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        URL urlHome = null;
+        for (String caminho : caminhosHome) {
+            urlHome = getClass().getResource(caminho);
+            if (urlHome != null) break;
         }
-    }
 
-    private void trocarTela(MouseEvent event, String caminhoFXML) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource(caminhoFXML));
-
-            Stage stage = (Stage) ((Node) event.getSource())
-                    .getScene()
-                    .getWindow();
-
+            if (urlHome == null) throw new IOException("Arquivo 'home-view.fxml' não encontrado.");
+            Parent root = FXMLLoader.load(urlHome);
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             stage.setScene(new Scene(root));
+            stage.setTitle("CondoSoft - Home");
             stage.show();
-
         } catch (IOException e) {
             e.printStackTrace();
         }
